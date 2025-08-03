279b1f771f4af8476d1378ccfc2f98c42c2924b7
AgoDe <agostino.defelice@k-digitale.com>
2025-08-03 12:12:17 +0200

    fix authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  protect_from_forgery with: :null_session
+
+  def authenticate_user!
+    unless user_signed_in?
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
+  
 end
